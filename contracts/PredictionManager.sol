3c271aa12696b1b5ff9511e29a111e0bd07b7664
mamoesta <31963563+mamoesta@users.noreply.github.com>
2021-11-24 14:57:17 -0800

    Cleaning up some of the functions and logic within predmanager

--- contracts/PredictionManager.sol
+++ contracts/PredictionManager.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract PredictionManager {
   address public owner = msg.sender;
-  uint predID = 1;
+  uint public predId = 1;
 
   fallback() external payable{}
   receive() external payable{}
@@ -12,13 +12,12 @@ contract PredictionManager {
     address payable bidAddr;
     address payable challengerAddr;
     uint bidAmount;
+    uint challengerAmount;
     uint bidOdds;
     string bidGameWinner;
-    uint challengerAmount;
     uint gameID;
     bool bidWin;
     bool hasChallenger;
-
     //bid is marked final once it's paid out.
     bool isFinal;
   }
@@ -27,46 +26,80 @@ contract PredictionManager {
   mapping (address => uint) public addressBook;
   mapping (uint => Prediction) public predictionList;
   
-  function receiveNewBid(Prediction memory pred) public payable {
-    require(addressBook[msg.sender] == 0, 'send a bid from a new address');
-    addressBook[msg.sender] = predID;
-    pred.bidWin = false;
-    pred.hasChallenger = true;
-    pred.isFinal = false;
-    predictionList[predID] = pred;
-    predID++;  
+  function hasActiveBid(address addr) public returns(bool){
+    //check to determine if an address has an open bid
+    return predictionList[addressBook[addr]].isFinal;
   }
+  function receiveNewBid(Prediction memory pred) public payable {
+    require(hasActiveBid(msg.sender) != true, 'This address already has an active bid.');
+
+    //if this address has yet to ever file a bid
+    if(addressBook[msg.sender] == 0){
+      // add a new entry
+      pred.bidWin = false;
+      pred.hasChallenger = false;
+      pred.isFinal = false;
+      predictionList[predId] = pred;
+      predId++;  
+    }
+    // this address is trying to add a new bid and the previous pred is finalized
+    else if (addressBook[msg.sender]!= 0 && predictionList[addressBook[msg.sender]].isFinal == true) {
+      pred.bidWin = false;
+      pred.hasChallenger = false;
+      pred.isFinal = false;
+      predictionList[addressBook[msg.sender]] = pred;
+    }
+    // this address is trying to add a new bid and the previous bid is not final
+    else{
+      //uncaught
+    }
+    
+  }
+
   function returnResults() public returns (bool) {
     address payable sourceAddr = payable(msg.sender);
-    uint index = addressBook[sourceAddr];
-    Prediction memory pred = predictionList[index];
+    Prediction memory pred = predictionList[addressBook[msg.sender]];
+    require(pred.isFinal,'Cannot return payouts for a matched bid until the results are final.');
+    
+    // We checked that the game is final, now we can reset isFinal so that addresses can issue fresh bids
+    predictionList[addressBook[msg.sender]].isFinal = false;
+    
     if(!pred.hasChallenger && sourceAddr == pred.bidAddr){
-      pred.bidAddr.transfer(pred.bidAmount );
-      //send original amount back to sourceAddr
+      uint amount = pred.bidAmount;
+      (bool sent, bytes memory data) = pred.bidAddr.call{value: amount}("");
+      require(sent, "Failed to send Ether");
+      return true;
     }
     else if (pred.bidAddr == sourceAddr && pred.bidWin){
-      uint winAmount = pred.bidAmount * pred.bidOdds;
-      pred.bidAddr.transfer(winAmount);
-      // send winAmount to bidder
+      uint winAmount = pred.bidAmount + pred.challengerAmount;
+      (bool sent, bytes memory data) = pred.bidAddr.call{value: winAmount}("");
+      require(sent, "Failed to send Ether");
+      return true;
     }
     else if (pred.challengerAddr == sourceAddr && !pred.bidWin) {
-      uint winAmount = pred.challengerAmount;
-      pred.challengerAddr.transfer(winAmount);
+      uint winAmount = pred.bidAmount + pred.challengerAmount;
+      (bool sent, bytes memory data) = pred.bidAddr.call{value: winAmount}("");
+      require(sent, "Failed to send Ether");
+      return true;
     }
     else {
       return false;
       // return the money to everyone - perhaps in the case of a tie or some other uncaught result
     }
-    return true;
   }
-  function updateBidWithAsk (uint predIndex, address payable challengeAddr, uint amount) public payable {
-    addressBook[challengeAddr] = predIndex;
-    predictionList[predIndex].challengerAddr = challengeAddr;
+  function updateBidWithChallenger (uint predIndex, address payable challengerAddr, uint amount) public payable {
+    addressBook[challengerAddr] = predIndex;
+    require(predictionList[predIndex].hasChallenger == false, 'This bid already has a challenger');
+    require(predictionList[predIndex].bidAmount == amount, 'Challenge amount does not match bid amount.');
+    
+    predictionList[predIndex].challengerAddr = challengerAddr;
     predictionList[predIndex].challengerAmount = amount;
-    predictionList[predIndex].isFinal = true;
-    //TODO ensure bid b * bid odds = ask amount
+    predictionlist[predIndex].hasChallenger = true;
   }
-  function getPred( uint index) public view returns (Prediction memory pred){
+  function getPred(uint index) public view returns (Prediction memory pred){
     return predictionList[index];
+  }
+  function getIndex(address addr) public view returns (uint index){
+    return addressBook[addr];
   }
 }
