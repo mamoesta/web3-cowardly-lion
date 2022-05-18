c34458c80d4e9782e68f05a4a6a683f9a43e9a14
marty.moesta <foo@bar.com>
2022-05-17 18:08:17 -0700

    enhancing the test

--- test/tests.js
+++ test/tests.js
@@ -1,12 +1,13 @@
 const { messagePrefix } = require("@ethersproject/hash");
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
-const hre = require("hardhat");
 
 
 describe("\nGetting started with Prediction and Game contracts\n", function (){
   let pred;
   let preds;
+  let game;
+  let games;
   let owner;
   let addr1;
   const bet_amount = ethers.utils.parseEther('100').toBigInt();
@@ -44,7 +45,7 @@ describe("\nGetting started with Prediction and Game contracts\n", function (){
   describe("\nPrediction Happy Path\n", function() {
     
     it("Create new bid, fill it with a challenger, finalize and pay out", async function (){
-      singlePred = {"bidAddr": owner.address,"challengerAddr": addr1.address, "bidAmount": bet_amount,"challengerAmount": bet_amount,"bidOdds": 10, "gameWinner": owner.address, "gameID": 0, "bidWin": true, "hasChallenger": true, "isFinal": true, "listPointer":0}
+      singlePred = {"bidAddr": owner.address,"challengerAddr": addr1.address, "bidAmount": bet_amount,"challengerAmount": bet_amount,"bidOdds": 50, "gameWinner": owner.address, "gameID": 0, "bidWin": false, "hasChallenger": true, "isFinal": true, "listPointer":0}
 
       singleGame = {"id":0,"homeTeam":"Giants","awayTeam":"Tigers","homeScore":10, "awayScore":6,"isFinal":true,"isLocked":true, "startTime":30303030}
 
@@ -56,14 +57,16 @@ describe("\nGetting started with Prediction and Game contracts\n", function (){
         await preds.newBid(singlePred, {value: bet_amount})
         cval = await preds.getMultiplier(owner.address);
         challenge = await preds.connect(addr1).updateBidWithChallenger(owner.address, addr1.address, {value: cval})
-        await preds.bidWin(owner.address)
+        
+        //await preds.bidWin(owner.address)
         //console.log("Prediction with challenger: ", await preds.predictions(owner.address))
 
-        console.log("Does owner address have a bid?: ", await preds.hasBid(owner.address))
+        //console.log("Does owner address have a bid?: ", await preds.hasBid(owner.address))
         await preds.returnResults(owner.address, 0)
-        console.log("Does owner address have a bid?: ", await preds.hasBid(owner.address))
-        console.log("Owner end balance: ",await owner.getBalance() )
-
+        //console.log("Does owner address have a bid?: ", await preds.hasBid(owner.address))
+        console.log("Bidder end balance: ", await owner.getBalance())
+        console.log("Challenger end balance: ", await addr1.getBalance())
+        expect(false).to.equal(await preds.hasBid(owner.address));
       }
       else {
         console.log("Invalid game! But the test still passed!")
